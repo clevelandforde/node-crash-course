76bb9911fccbc529e5bfb2660072e34f1af0c0e6
CLForde <fordecleveland4@gmail.com>
2022-03-10 04:13:04 -0400

    Add global object

---
+++ global.js
@@ -0,0 +1,15 @@
+// global objest
+
+// console.log(global);
+
+// global.setTimeout(() => {
+//   console.log('stop');
+//   clearInterval(beep);
+// }, 3000);
+
+// const beep = setInterval(() => {
+//   console.log('beep');
+// }, 1000);
+
+console.log(__dirname);
+console.log(__filename);
