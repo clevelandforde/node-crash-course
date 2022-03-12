9b3247b97edd3bf77311eb2acef67ea3471404aa
CLForde <fordecleveland4@gmail.com>
2022-03-12 09:15:22 -0400

    Tutorial #4 request object

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-  console.log('request made'); // This is log here in the console after going to localhost:port but it's not log in the console in the browser and that is because this console.log code is running on the server in the backend and not in the browser on the frontend.
+  console.log(req.socket);
 });
 
 const port = 3002;
