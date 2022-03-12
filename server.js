6813f17420e79a018eadce121273b58384ab2b9a
CLForde <fordecleveland4@gmail.com>
2022-03-12 10:43:27 -0400

    Demo response object

--- server.js
+++ server.js
@@ -1,10 +1,17 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-  console.log(req.socket);
+  console.log(req.url, req.method);
+
+  //Set header content type
+  res.setHeader('Content-Type', 'text/html');
+
+  // res.write('GDF');
+  res.write('\nForms');
+  res.end();
 });
 
-const port = 3002;
+const port = 3000;
 
 server.listen(port, () => {
   console.log(`Listening on port ${port}`);
