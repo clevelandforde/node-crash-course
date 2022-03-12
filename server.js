c3ae6c8f1f65275fb720fb6011e2a41b1f6a9b79
CLForde <fordecleveland4@gmail.com>
2022-03-12 12:23:19 -0400

    Returning html pages

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const http = require('http');
+const fs = require('fs');
 
 const server = http.createServer((req, res) => {
   console.log(req.url, req.method);
@@ -6,9 +7,13 @@ const server = http.createServer((req, res) => {
   //Set header content type
   res.setHeader('Content-Type', 'text/html');
 
-  // res.write('GDF');
-  res.write('\nForms');
-  res.end();
+  // send and html file
+  fs.readFile('./views/index.html', (err, result) => {
+    if (err) {
+      console.log(err);
+    } //res.write(result); // If we're only sending one thing into res.write, then we don't actually need this line of code here, we can put it directly into res.send method instead. If we were writing multiple things we might use res.write one after the other
+    else res.end(result);
+  });
 });
 
 const port = 3000;
