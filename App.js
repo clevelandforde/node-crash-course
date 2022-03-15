a9fcdcdee1f02f2fa8730726c5c80770314c677d
CLForde <fordecleveland4@gmail.com>
2022-03-15 19:59:51 -0400

    Tutorial #7 view engine
    Delete raw html pages
    Delete server.js
    Install ejs
    Add ejs files
    Edit App.js

--- App.js
+++ App.js
@@ -1,31 +1,33 @@
-// Express file is typically called App.js
-// Note additionally that we still keep our raw node server.js file up to this stage, simply for comparison with oun now express App.js file
 const express = require('express');
 
 // express app
 const app = express();
 
+// register view engine
+app.set('view engine', 'ejs');
+
 // listening for request
 app.listen(3000, () => {
   console.log('listening on port 3000 through express');
-}); // This creates an instance of a server and can be stored in a const like const serve = app.listen() for when we want to use web sockets
-
-app.get('/', (req, res) => {
-  //   res.send({ name: 'cleveland' });
-  res.sendFile('./views/index.html', { root: __dirname }); // This path (the first argument/parameter) originally is not a relative path, you will have to specifically state where it is relative from in the second parameter because by default it's going to look for and absolute path (the path from the root file computer). Therefore, the second argument that we have to pass is an options object and specify what the root should be which is the current directory that the app.js is in and to get the current directory we use __dirname.
-}); // This is how we respond to the request we listening for by setting routes/endpoints. The app.get() method takes in two arguments; 1 the path and 2 a function that takes in a request and response object so that we can do something with these objects. the req contains info about the request such as the url and the method of the request like get, post, put etc and res is the response object which we can use to send a response.
-
-app.get('/about', (req, res) => {
-  res.sendFile('./views/about.html', { root: __dirname });
 });
 
-// redirects
-app.get('/about-us', (req, res) => {
-  res.redirect('/about');
+app.get('/', (req, res) => {
+  //   res.sendFile('./views/index.html', { root: __dirname }); // We no longer need the line with res.sendFile since we are now using ejs. If we just replace .html with .ejs then it will take us to downloads for us to do a save. At this 15th day of March 2022 I cannot explain why it takes me to my download folder, I will explain that as soon as i understand. Henceforth, we need res.render(path) seen below, to render a view and send it back to the browser.
+  //   res.render('index.ejs'); // This wasn't working without the extension and now i realized that i need to register view engine above using app.set('view engine', 'ejs')
+  res.render('index', { title: 'Home' }); // no need for extension since we've now register view engine above. Here express is going to look inside the views folder, find index.ejs, use the ejs view engine to render this index.ejs and send it back to the browser
+});
+
+app.get('/about', (req, res) => {
+  //   res.sendFile('./views/about.html', { root: __dirname });
+  res.render('about', { title: 'About' });
+});
+
+app.get('/registrations/create', (req, res) => {
+  res.render('create', { title: 'Create new registration' });
 });
 
 // 404 page
 app.use((req, res) => {
   // It is not scoped out to a particular url like '/404'
-  res.status(404).sendFile('./views/404.html', { root: __dirname }); // Here we have to manually set the status code
+  res.status(404).render('404', { title: '404' });
 });
